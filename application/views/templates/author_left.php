c188d0a6e39dd163958f5199e045228e45ffb965
luke18 <s_tao16@sina.com>
2013-11-18 15:08:42 +0800

    single blog

---
+++ application/views/templates/author_left.php
@@ -0,0 +1,6 @@
+<aside class="u-1-5">
+
+    <p><?= $blog['author'] ?></p>
+    <p><?= $blog['time'] ?></p>
+
+</aside>
--- application/views/templates/blog_single.php
+++ application/views/templates/blog_single.php
@@ -1,9 +1,12 @@
 <div class="g-r fw-wrapper">
-    <?php include 'application/views/templates/cover.php' ?>
-    <section class="u-2-3 wd-wrapper" tabindex="-1">
+    <?php include 'application/views/templates/author_left.php' ?>
+
+    <section class="u-3-5" tabindex="-1">
 
         <div class="wd-main" id="wd-main">
             <?php include 'application/views/templates/blog_single_content.php' ?>
         </div>
+    </section>
+    <section class="u-1-5">
     </section>
 </div>
