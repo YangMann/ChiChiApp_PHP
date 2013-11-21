10f7644d73aa97e79a33bbce7a28f5abe2589e8a
yamamaki <dingzhm93@gmail.com>
2013-11-21 17:01:02 +0800

    implement gcm service by httpclient.class

--- application/controllers/gcm.php
+++ application/controllers/gcm.php
@@ -5,15 +5,17 @@
  * Date: 13-11-18
  * Time: 12:14
  */
+
 define("GOOGLE_API_KEY", "AIzaSyA4g71gAcTsRnmIE78n9iSdlvTqwLDCAcM");
 define("GOOGLE_GCM_URL", "https://android.googleapis.com/gcm/send");
+require_once 'HttpClient.class.php';
 
 class Gcm extends CI_Controller {
 
     function __construct() {
         parent::__construct();
 
-        $this->load->helper('url');
+        $this->load->helpers('url');
     }
 
     function index() {
@@ -25,26 +27,50 @@ class Gcm extends CI_Controller {
             'registration_ids' => array($reg_id),
             'data' => array("message" => $message)
         );
-        $headers = array(
-            'Authorization: key='.GOOGLE_API_KEY,
-            'Content-Type: application/json'
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, GOOGLE_GCM_URL);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-
-        $result = curl_exec($ch);
-        if ($result === FALSE) {
-            die('Problem occured: '.curl_error($ch));
-        }
-
-        curl_close($ch);
-        echo $result;
+        $pageContents = HttpClient::quickPost(GOOGLE_GCM_URL, $fields);
+        echo $pageContents;
+////        echo json_encode($fields);
+//        $data = urldecode(http_build_query($fields));
+//        echo $data;
+//        $params = array(
+//            'http' => array(
+//                'method' => 'POST',
+//                'header' => array(
+//                    'Authorization: key='.GOOGLE_API_KEY,
+//                    'Content-Type: application/x-www-form-urlencoded'
+//                ),
+//                'content' => $data
+//            )
+//        );
+//        echo json_encode($params);
+//        print_r($params);
+//        $context = stream_context_create($params);
+//        echo $context;
+//        $result = file_get_contents(GOOGLE_GCM_URL, false, $context);
+//        $headers = array(
+//            'Authorization: key='.GOOGLE_API_KEY,
+//            'Content-Type: application/json'
+//        );
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, GOOGLE_GCM_URL);
+//        curl_setopt($ch, CURLOPT_POST, true);
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+//        $result = curl_exec($ch);
+//        $data = array(
+//            'http' => array(
+////                'method' => 'POST',
+//                'header' => $headers,
+//                'content' => $fields
+//            )
+//        );
+//        if ($result === false) {
+////            die('Problem occured: '.curl_error($ch));
+//        }
+//        curl_close($ch);
+//        echo $result;
+//        return $result;
     }
-
 }
