747561c5ad8964e4a57274599079d8117c52f3a6
luke18 <s_tao16@sina.com>
2013-11-22 14:03:34 +0800

    php markdown

--- application/views/templates/blog_single.php
+++ application/views/templates/blog_single.php
@@ -1,28 +1,32 @@
 <div class="g-r fw-wrapper">
-    <section class="u-1-5">
-    </section>
-    <section class="u-3-5" tabindex="-1">
+    <!-- could use include php but how to change tags-->
+    <aside class="u-1-3 as-cover wd-fullscreen" data-cover-display="hidden">
+        <div class="as-cover-img"></div>
+        <div class="as-cover-img as-cover-img-b transparent"></div>
+        <div class="as-cover-body-wrapper">
+            <div class="as-cover-body">
+                <h1 class="as-cover-title">交大吃吃</h1>
 
-        <div class="bl-view-wrapper">
+                <p class="as-cover-description">两岸猿声啼不住，轻舟已过万重山</p>
+
+                <div class="as-cover-actions">
+                    <a class="bt bt-default" href="#">jAccount登陆</a>
+                    <a class="bt bt-primary">人人登陆</a>
+                </div>
+
+                <footer class="as-cover-annotations">
+                    <a class="as-cover-name" href="#">韩式石锅拌饭</a>
+                    <a class="as-cover-building">五餐</a>
+                    <a class="as-cover-restaurant">韩国餐厅</a>
+                    <a class="as-cover-copyright">交大吃吃，SJTU</a>
+                </footer>
+            </div>
+        </div>
+    </aside>
+
+    <section class="u-2-3 wd-wrapper wd-fullscreen" tabindex="-1">
+        <div class="wd-main" id="wd-main">
             <?php include 'application/views/templates/blog_single_content.php' ?>
         </div>
     </section>
-
-    <?php include 'application/views/templates/author_left.php' ?>
 </div>
-
-<footer class="bl-footer">
-    <div class="bl-preview">
-        <a href="/blog/<?= $blog_next['id']?>" class="bl-background-size-cover">
-            <div class="bl-preview-content">
-                <div class="bl-preview-description">
-                    Read next
-                </div>
-                <h3 class="bl-preview-title">
-                    <?=$blog_next['title']?>
-                </h3>
-            </div>
-            <img class="bl-header-image-cover" src=<?= $blog_next['feature_img']?>>
-        </a>
-    </div>
-</footer>
