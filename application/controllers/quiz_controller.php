eaad1bbae9c0b3c1036e5773130463a3da44b53c
yamamaki <dingzhm93@gmail.com>
2013-11-22 12:20:04 +0800

    implement quiz function with database

--- application/controllers/quiz_controller.php
+++ application/controllers/quiz_controller.php
@@ -12,13 +12,39 @@ class Quiz_Controller extends CI_Controller
     {
         $this->load->helper(array('form', 'url'));
         $this->load->library('form_validation');
-
-        $data['question_id'] = $question_id;
         $this->load->model('questions_model');
+
+        $former_question_id = $question_id - 1;
+        $next_question_id = $question_id + 1;
+        $data['question_id'] = $question_id;
+        $data['former_question_id'] = $former_question_id;
+        $data['next_question_id'] = $next_question_id;
+
         $question = $this->questions_model->get_question($question_id);
-        $data['question'] = $question;
-        $data['next_question'] = $question_id+1;
-        print_r($data);
-        $this->load->view('onlineQuiz', $data);
+        $data['question'] = $question[0]['question'];
+
+        if ($this->input->post()) {
+            $data['answer'] = $_POST['answer'];
+            $answer = $data['answer'];
+            $this->questions_model->answer(1, $former_question_id, $answer);
+        } else {
+            $data['answer'] = '';
+        }
+        $this->load->view('online_quiz', $data);
+
+//        if ($this->questions_model->is_question_empty($former_question_id)) {
+//            echo 'it is the first question';
+//            $this->load->view('online_quiz', $data);
+//        }
+//        else if ($this->questions_model->is_question_empty($next_question_id)) {
+//            echo 'it is the last question';
+//
+//            $this->load->view('online_quiz', $data);
+//        }
+//        else {
+//            $this->load->view('online_quiz', $data);
+//        }
+
+
     }
 }
--- application/models/questions_model.php
+++ application/models/questions_model.php
@@ -11,13 +11,54 @@ class questions_model extends CI_Model {
     public function __construct() {
         $this->load->database();
     }
+
+    public function is_question_empty($question_id)
+    {
+        if (get_question($question_id)) return FALSE;
+        else return TRUE;
+    }
+
     public function get_questions() {
-        return $this->db->get('questions_table')->result_array();
+        return $this->db->get('adventure_questions')->result_array();
     }
     public function get_question($question_id) {
-        $this->db->select('question_id, description');
-        $this->db->from('questions_table');
-        $this->db->where('question_id', $question_id);
-        return $this->db->get()->result_array();
+//        $this->db->select('id, question, score, genre, answer');
+//        $this->db->from('adventure_questions');
+//        $this->db->where('id', $question_id);
+//        return $this->db->get()->result_array();
+        $sql = "SELECT * FROM adventure_questions WHERE id=" . $this->db->escape($question_id);
+        $query = $this->db->query($sql);
+        return $query->result_array();
+    }
+
+    public function is_answer_empty($user_id, $answer_id)
+    {
+        $sql = "SELECT * FROM users_answers WHERE user_id=" . $this->db->escape($user_id) . " AND answer_id=" . $this->db->escape($answer_id);
+        $query = $this->db->query($sql);
+        $result = $query->result();
+        if (!$result) return TRUE;
+        else return FALSE;
+    }
+
+    public function insert_answer($user_id, $answer_id, $answer)
+    {
+        $sql = "INSERT INTO users_answers (user_id, answer_id, answer)
+        VALUES (" . $this->db->escape($user_id) . ", " . $this->db->escape($answer_id) . ", " . $this->db->escape($answer) . ")";
+        $this->db->query($sql);
+    }
+
+    public function update_answer($user_id, $answer_id, $answer)
+    {
+        $sql = "UPDATE users_answers SET answer = " . $this->db->escape($answer) . " WHERE user_id = " . $this->db->escape($user_id) . " AND answer_id = " . $this->db->escape($answer_id);
+        $this->db->query($sql);
+    }
+
+    public function answer($user_id, $answer_id, $answer)
+    {
+        if (questions_model::is_answer_empty($user_id, $answer_id)) {
+            questions_model::insert_answer($user_id, $answer_id, $answer);
+        } else {
+            questions_model::update_answer($user_id, $answer_id, $answer);
+        }
     }
 }
