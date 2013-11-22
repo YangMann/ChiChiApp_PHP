49201bf30597097cd989c6ebfc6a9181bd9d5089
etflly <etflly@gmail.com>
2013-11-23 07:19:32 +0800

    bug fix

--- assets/js/app/ng/main.js
+++ assets/js/app/ng/main.js
@@ -2,12 +2,17 @@
  * Created by JeffreyZhang on 13-10-23.
  */
 $(document).ready(function(){
-    $(window).resize(function(){
-        if($(window).width()>=767)
+    function u23resize(){
+        //if($(window).width()>=767){
+        if($(".u-1-3").css("visibility")=="visible" && $(".u-1-3").css("display")!="none") {
             $(".u-2-3").css("width", $(window).width()-$(".u-1-3").width());
-        else
+            $(".nv-open-all").css("width", $(".u-1-3").width());
+        }
+        else{
             $(".u-2-3").css("width", $(window).width());
-    });
+        }
+    }
+    $(window).resize("u23resize()");
     $(window).resize();
 });
 
